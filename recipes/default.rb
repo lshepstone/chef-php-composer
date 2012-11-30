c82369065bcbe4e5995f08e9aec0d81f1f2aa9a1
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2012-11-30 14:28:46 +0000

    Reformatted the platform when switch.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,12 +5,12 @@ package "curl" do
 end
 
 case node['platform']
-  when "ubuntu","debian"
-    bash "install_composer" do
-      cwd "#{Chef::Config[:file_cache_path]}"
-      code <<-EOH
-        curl -s #{node[:composer][:url]} | php
-        mv composer.phar #{node[:composer][:path]}
-      EOH
-    end
+when "ubuntu","debian"
+  bash "install_composer" do
+    cwd "#{Chef::Config[:file_cache_path]}"
+    code <<-EOH
+      curl -s #{node[:composer][:url]} | php
+      mv composer.phar #{node[:composer][:path]}
+    EOH
+  end
 end
